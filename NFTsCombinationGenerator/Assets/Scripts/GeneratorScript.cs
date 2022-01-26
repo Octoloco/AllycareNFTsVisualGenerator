e3d3ff11a11f5dfaceabdfc049027a6969d82ded
Diego Clorio <diego_clorio@hotmail.com>
2022-01-25 19:14:37 -0600

    Started generator implementation

---
+++ NFTsCombinationGenerator/Assets/Scripts/GeneratorScript.cs
@@ -0,0 +1,32 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+public class GeneratorScript : MonoBehaviour
+{
+    [SerializeField]
+    private TextMeshProUGUI numberOfNFTs;
+    [SerializeField]
+    private Sprite[] planets;
+    [SerializeField]
+    private Sprite[] moons;
+    [SerializeField]
+    private Sprite[] rings;
+    [SerializeField]
+    private Sprite[] backgrounds;
+    [SerializeField]
+    private Sprite[] cards;
+    [SerializeField]
+    private Sprite[] words;
+
+    void Start()
+    {
+        
+    }
+
+    void Update()
+    {
+
+    }
+}
